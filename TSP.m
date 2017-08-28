ecae0cb1f27d124a95d7126bad12670a6f8cadbf
unknown <green.koopa667@gmail.com>
2017-08-28 03:07:04 -0400

    Optimized code. Can run 12 with some effort

--- TSP.m
+++ TSP.m
@@ -9,41 +9,54 @@ tic;
 % Parse coordinates from sample .tsp file
 coords = ReadFromSample(filename);
 
-% Grab coordinates aside from first city to permute (starting city will
-% remain the same)
-toPermute = coords(2:end);
-
-routes(1:factorial(size(coords,2)-1),1) = coords(1);
-% Find permutations
-disp('Starting Permutations');
-routes(:,2:size(coords,2)) = perms(toPermute);
-disp('Permutations Done');
-
-dist_totals = zeros(size(routes,1),1);
-
-% Calculate distances for each route
-for j = 1:size(routes,1)
-    dist = 0;
-    for k = 1:(size(routes,2))
-        if k == size(routes,2)
-            coord1 = routes(j,k);
-            coord2 = routes(j,1);
-        else
-            coord1 = routes(j,k);
-            coord2 = routes(j,k+1);
-        end
-        dist = dist + Distance(coord1, coord2);
-    end
-    dist_totals(j) = dist;
+% Create indexes for permutations for everything aside from the first city
+for i = 2:size(coords,2)
+    toPermute(i-1) = i;
 end
 
-% Get path with shortest distance
-[~, win_index] = min(dist_totals);
-clearvars dist_totals
-win = routes(win_index, :);
+% Instead of storing all coordinates, do permuations of the index
+% Find permutation indices
+disp('Starting Permutations');
+subroutes = single(perms(toPermute));
+disp('Permutations Done');
+
+% Arbitrarily large number to begin with
+distMin = inf(1);
+
+% Calculate distances for each route
+for j = 1:size(subroutes,1)
+    dist = 0;
+    for k = 1:(size(subroutes,2)+1)
+        if k == 1
+            coord1 = coords(1);
+            coord2 = coords(subroutes(j,1));
+        elseif k == size(subroutes,2)+1
+            coord1 = coords(subroutes(j,k-1));
+            coord2 = coords(1);
+        else
+            coord1 = coords(subroutes(j,k-1));
+            coord2 = coords(subroutes(j,k));
+        end             
+        dist = dist + Distance(coord1, coord2);
+    end
+    % Replace lowest distance if better path found
+    if dist < distMin
+        distMin = dist;
+        minPerm = subroutes(j,:);
+    end
+end
+
+% Use minimum distance and path indices to create path for plotting
+pathIndices = [1 minPerm];
+for i = 1:size(pathIndices,2)
+    winningPath(i) = coords(pathIndices(i));
+end
+
+% Slap starting city on the end
+winningPath = [winningPath coords(1)];
 
 % Plot points and winning path
-PlotPoints(coords, win);
+PlotPoints(coords, winningPath);
 
 toc;
 
