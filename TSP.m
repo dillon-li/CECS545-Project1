aae56fe7370a91dae15c8020f717cf25d3ce838f
unknown <green.koopa667@gmail.com>
2017-08-29 01:45:16 -0400

    Now fully efficient with own algorithm

--- TSP.m
+++ TSP.m
@@ -23,33 +23,60 @@ end
 % Instead of storing all coordinates, do permuations of the index
 % Find permutation indices
 disp('Starting Permutations');
-subroutes = single(perms(toPermute));
-disp('Permutations Done');
+% subroutes = single(perms(toPermute));
 
 % Arbitrarily large number to begin with
 distMin = inf(1);
 
-% Calculate distances for each route
-for j = 1:size(subroutes,1)
-    dist = 0;
-    for k = 1:(size(subroutes,2)+1)
-        if k == 1
-            x1 = x(1); y1 = y(1);
-            x2 = x(subroutes(j,1)); y2 = y(subroutes(j,1));
-        elseif k == size(subroutes,2)+1
-            x1 = x(subroutes(j,k-1)); y1 = y(subroutes(j,k-1));
-            x2 = x(1); y2 = y(1);
+% Implement Heap's Algorithm
+c = ones(size(toPermute,2),1);
+i = 1;
+permChange = 1;
+% Calculate distances for each route as they are found 
+while i < size(toPermute,2)+1
+    if permChange == 1
+        % Loop througuh permutation to calculate distances
+        dist = 0;
+        for k = 1:size(toPermute,2)+1
+            if k == 1
+                x1 = x(1); y1 = y(1);
+                x2 = x(toPermute(k)); y2 = y(toPermute(k));
+            elseif k == size(toPermute,2)+1
+                x1 = x(toPermute(k-1)); y1 = y(toPermute(k-1));
+                x2 = x(1); y2 = y(1);
+            else
+            	x1 = x(toPermute(k-1)); y1 = y(toPermute(k-1));
+                x2 = x(toPermute(k)); y2 = y(toPermute(k));
+            end             
+            dist = dist + Distance(x1,y1,x2,y2);
+        end
+        % Replace lowest distance if better path found
+        if dist < distMin
+            distMin = dist;
+            minPerm = toPermute;
+        end
+    end
+    % Next permutation using Heap's Algorithm
+    if c(i) < i
+        if mod(i+1,2) == 0
+            temp = toPermute(1);
+            toPermute(1) = toPermute(i);
+            toPermute(i) = temp;
         else
-            x1 = x(subroutes(j,k-1)); y1 = y(subroutes(j,k-1));
-            x2 = x(subroutes(j,k)); y2 = y(subroutes(j,k));
-        end             
-        dist = dist + Distance(x1,y1,x2,y2);
+            temp = toPermute(c(i));
+            toPermute(c(i)) = toPermute(i);
+            toPermute(i) = temp;
+        end
+        % toPermute is now in format of next permutation
+        c(i) = c(i) + 1;
+        i = 1;
+        permChange = 1;
+    else
+        c(i) = 1;
+        i = i + 1;
+        permChange = 0;
     end
-    % Replace lowest distance if better path found
-    if dist < distMin
-        distMin = dist;
-        minPerm = subroutes(j,:);
-    end
+
 end
 
 % Use minimum distance and path indices to create path for plotting
@@ -58,11 +85,14 @@ for i = 1:size(pathIndices,2)
     winningPath(i) = coords(pathIndices(i));
 end
 
+disp('Permutations Done');
+
 % Slap starting city on the end
 winningPath = [winningPath coords(1)];
 
 % Append starting city to end
 pathIndices = [pathIndices 1];
+
 
 time = toc; % Time after distance and permutations calculated
 
